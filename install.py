8cfc947ae0bd1775f3fd898cc1dd1c23ad42a88e
iiseppi <47714239+iiseppi@users.noreply.github.com>
2026-01-09 19:07:31 +0200

    Update install.py

--- install.py
+++ install.py
@@ -11,7 +11,7 @@ from io import StringIO
 
 #-------- extension info -----------
 
-VERSION      = "0.3.49"
+VERSION      = "0.5"
 NAME         = 'wlip-emulator'
 DESCRIPTION  = 'wlip emulator'
 AUTHOR       = "iiseppi"
@@ -42,18 +42,22 @@ class WundergroundLikeInstaller(ExtensionInstaller):
 extension_config = """
 
 [WeatherLinkEmulator]
-    # The port to listen on (Davis default is 22222)
+    # 1. Default Port
+    # This port (22222) is open to all connections.
+    # Should work with WeatherCat, Weather Display and CumulusMX simultaneusly.
     port = 22222
-    
-    # Maximum concurrent clients
-    max_clients = 5
-    
-    # Optional: Force the reported archive interval in minutes.
-    # IMPORTANT: If your client software (e.g. WeatherCat) expects 1 minute
-    # but WeeWX is set to 5 minutes, force this to 1. 
-    # In many cases, you can leave this commented out to use WeeWX's archive interval.
-    # archive_interval = 1
 
+    # 2. Client Mapping (VIP Ports)
+    # Format: IP_ADDRESS:PORT
+    # This maps specific IPs to dedicated ports.
+    # The line below opens port 22223 ONLY for 192.168.1.2 (Your WeeWX client).
+    # All Weewx clients should be mapped here to their own VIP port.
+    # Multiple clients can be mapped by adding additional lines.
+    client_mapping = 192.168.1.2:22223
+
+    # General settings
+    max_clients = 10
+    binding = wx_binding
 """
 config_dict = configobj.ConfigObj(StringIO(extension_config))
 
